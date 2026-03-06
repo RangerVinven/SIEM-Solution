bc9873884a12093ae2687501dc2a595ed6bee494
Daniel McPherson <daniel.mcpherson@live.co.uk>
2026-03-06 00:20:44 +0000

    Added shared-utils and shared-filters

--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -3,3 +3,5 @@ rootProject.name = "siem-backend"
 // Services and modules
 include("services:account-service")
 include("common:shared-models")
+include("common:shared-utils")
+include("common:shared-filters")
