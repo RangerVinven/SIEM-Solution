65d4107bfdc31d9d18f3a79c4aacd05d47068147
Daniel McPherson <daniel.mcpherson@live.co.uk>
2026-03-11 12:21:39 +0000

    Added updateEntity and toEntity methods

--- backend/services/account-service/src/main/java/siem/account/mapper/UserMapper.java
+++ backend/services/account-service/src/main/java/siem/account/mapper/UserMapper.java
@@ -2,8 +2,11 @@ package siem.account.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
+import siem.account.dto.AddEmployeeRequest;
 import siem.account.dto.CreateUserRequest;
+import siem.account.dto.UpdateUserRequest;
 import siem.account.dto.UserResponse;
 import siem.account.entity.User;
 
@@ -14,5 +17,18 @@ public interface UserMapper {
     @Mapping(target = "passwordHash", ignore = true)
     User toEntity(CreateUserRequest request);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "passwordHash", ignore = true)
+    @Mapping(target = "organisationId", ignore = true)
+    User toEntity(AddEmployeeRequest request);
+
     UserResponse toResponse(User entity);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "email", ignore = true)
+    @Mapping(target = "passwordHash", ignore = true)
+    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "organisationId", ignore = true)
+    void updateEntity(UpdateUserRequest request, @MappingTarget User entity);
 }
