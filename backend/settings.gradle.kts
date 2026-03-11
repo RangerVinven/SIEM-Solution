0cc598c4d1a65ad1798ad1dc61ae4b6e51a552b6
Daniel McPherson <daniel.mcpherson@live.co.uk>
2026-03-11 14:30:35 +0000

    Renamed aggregator to aggregation

--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -2,7 +2,7 @@ rootProject.name = "siem-backend"
 
 // Services and modules
 include("services:account-service")
-include("services:aggregator-service")
+include("services:aggregation-service")
 include("common:shared-models")
 include("common:shared-utils")
 include("common:shared-filters")
