f3597eb85764f2891151304318d9a9384d3fd369
Daniel McPherson <daniel.mcpherson@live.co.uk>
2026-03-12 16:34:53 +0000

    Changed SiemEvent to RawSiemEvent

--- backend/services/aggregation-service/src/main/java/siem/aggregator/controller/AggregatorController.java
+++ backend/services/aggregation-service/src/main/java/siem/aggregator/controller/AggregatorController.java
@@ -1,7 +1,7 @@
 package siem.aggregator.controller;
 
 import lombok.RequiredArgsConstructor;
-import siem.models.SiemEvent;
+import siem.models.RawSiemEvent;
 import siem.aggregator.service.EventPublisherService;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +15,7 @@ public class AggregatorController {
     private final EventPublisherService service;
 
     @PostMapping("/v1/aggregate")
-    public void aggregate(@RequestBody SiemEvent eventPayload) {
+    public void aggregate(@RequestBody RawSiemEvent eventPayload) {
         service.publishEvent(eventPayload);
     }
 }
