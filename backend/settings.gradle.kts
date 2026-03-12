119ecf6b853444e13c8fefe610ed0bd61d875ca9
Daniel McPherson <daniel.mcpherson@live.co.uk>
2026-03-12 16:35:23 +0000

    Added normalisation-service

--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "siem-backend"
 // Services and modules
 include("services:account-service")
 include("services:aggregation-service")
+include("services:normalisation-service")
 include("common:shared-models")
 include("common:shared-utils")
 include("common:shared-filters")
