e661144147a6a62a19c23877451e594a76c571ea
Daniel McPherson <daniel.mcpherson@live.co.uk>
2026-03-12 16:34:26 +0000

    Renamed group

--- backend/services/aggregation-service/build.gradle.kts
+++ backend/services/aggregation-service/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "siem.aggregator"
+group = "siem.aggregation"
 version = "0.0.1-SNAPSHOT"
 description = "The log aggregator"
 
