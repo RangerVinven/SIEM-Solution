bd1607063a7b7929a13c9b556a363e5baeadbc1f
Daniel McPherson <daniel.mcpherson@live.co.uk>
2026-03-12 16:35:13 +0000

    Added error handling

--- backend/services/aggregation-service/src/main/java/siem/aggregator/service/EventPublisherService.java
+++ backend/services/aggregation-service/src/main/java/siem/aggregator/service/EventPublisherService.java
@@ -4,15 +4,20 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
-import siem.models.SiemEvent;
+import siem.models.RawSiemEvent;
 
 @Service
 @RequiredArgsConstructor
 public class EventPublisherService {
     
-    private final KafkaTemplate<String, SiemEvent> kafkaTemplate;
+    private final KafkaTemplate<String, RawSiemEvent> kafkaTemplate;
 
-    public void publishEvent(SiemEvent event) {
-        kafkaTemplate.send("events", event);
+    public void publishEvent(RawSiemEvent event) {
+        kafkaTemplate.send("raw-logs", event)
+        .whenComplete((result, ex) -> {
+            if (ex != null) {
+                System.err.println("Failed to publish event: " + ex.getMessage());
+            }
+        });
     }
 }
