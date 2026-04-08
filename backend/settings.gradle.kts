12fcd92f9f15a73372877641c1461d01d5054e95
Daniel McPherson <daniel.mcpherson@live.co.uk>
2026-04-08 18:34:16 +0100

    Added services

--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -1,9 +1,14 @@
 rootProject.name = "siem-backend"
 
-// Services and modules
 include("services:account-service")
 include("services:aggregation-service")
 include("services:normalisation-service")
+include("services:log-saving-service")
+include("services:log-query-service")
+include("services:log-analysis-service")
+include("services:notification-service")
+include("services:alert-service")
+include("services:agent-service")
 include("common:shared-models")
 include("common:shared-utils")
 include("common:shared-filters")
